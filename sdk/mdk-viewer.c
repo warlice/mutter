271420ce98d94c1391a5770e09f4acaae9c753ce
Jonas Ådahl <jadahl@gmail.com>
2021-01-23 08:37:51 +0100

    Add empty sdk application
    While just being a header bar and an about dialog, the aim of this
    application is to provide a more useful interface for running mutter and
    gnome-shell as a nested display server, for debugging purposes.
    
    The icon used for now is 'default_window.png', as well as the same image
    scaled to 64x64 and 128x128.
    
    In contrast to the meta-*/Meta* prefix, the SDK application has the
    mdk-*/Mdk* prefix, standing for Meta/Mutter Development Kit.

---
+++ sdk/mdk-viewer.c
@@ -0,0 +1,124 @@
+/*
+ * Copyright (C) 2021 Red Hat Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "config.h"
+
+#include <glib/gi18n-lib.h>
+#include <gtk/gtk.h>
+
+static void
+activate_about (GSimpleAction *action,
+                GVariant      *parameter,
+                gpointer       user_data)
+{
+  GtkApplication *app = user_data;
+  const char *authors[] = {
+    _("The Mutter Team"),
+    NULL
+  };
+
+  gtk_show_about_dialog (GTK_WINDOW (gtk_application_get_active_window (app)),
+                         "program-name", _("Mutter SDK"),
+                         "version", VERSION,
+                         "copyright", "© 2001—2021 The Mutter Team",
+                         "license-type", GTK_LICENSE_GPL_2_0,
+                         "website", "http://gitlab.gnome.org/GNOME/mutter",
+                         "comments", _("Mutter and GNOME Shell software development kit"),
+                         "authors", authors,
+                         "logo-icon-name", "org.gnome.Mutter.Sdk",
+                         "title", _("About Mutter SDK"),
+                         NULL);
+}
+
+static void
+activate (GApplication *app)
+{
+  g_autoptr (GtkBuilder) builder = NULL;
+  GtkWidget *window;
+
+  builder = gtk_builder_new_from_resource ("/ui/mdk-viewer.ui");
+
+  window = GTK_WIDGET (gtk_builder_get_object (builder, "window"));
+  gtk_application_add_window (GTK_APPLICATION (app), GTK_WINDOW (window));
+  gtk_widget_show (window);
+}
+
+static void
+print_version (void)
+{
+  g_print ("mutter-viewer %s\n", VERSION);
+}
+
+static int
+local_options (GApplication *app,
+               GVariantDict *options,
+               gpointer      data)
+{
+  gboolean version = FALSE;
+
+  g_variant_dict_lookup (options, "version", "b", &version);
+
+  if (version)
+    {
+      print_version ();
+      return 0;
+    }
+
+  return -1;
+}
+
+int
+main (int    argc,
+      char **argv)
+{
+  g_autoptr (GtkApplication) app = NULL;
+  static GActionEntry app_entries[] = {
+    { "about", activate_about, NULL, NULL, NULL },
+  };
+  struct {
+    const char *action_and_target;
+    const char *accelerators[2];
+  } accels[] = {
+    { "app.about", { "F1", NULL } },
+  };
+  int i;
+
+  g_set_prgname ("org.gnome.Mutter.Sdk");
+  app = gtk_application_new ("org.gnome.Mutter.Sdk",
+                             G_APPLICATION_NON_UNIQUE);
+
+  g_action_map_add_action_entries (G_ACTION_MAP (app),
+                                   app_entries, G_N_ELEMENTS (app_entries),
+                                   app);
+  for (i = 0; i < G_N_ELEMENTS (accels); i++)
+    {
+      gtk_application_set_accels_for_action (app,
+                                             accels[i].action_and_target,
+                                             accels[i].accelerators);
+    }
+
+  g_application_add_main_option (G_APPLICATION (app),
+                                 "version", 0, 0, G_OPTION_ARG_NONE,
+                                 "Show version", NULL);
+
+  g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
+  g_signal_connect (app, "handle-local-options", G_CALLBACK (local_options), NULL);
+
+  g_application_run (G_APPLICATION (app), argc, argv);
+
+  return EXIT_SUCCESS;
+}
