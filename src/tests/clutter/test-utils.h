c0ef1cd368b113d60a4f427f812f8e5372d4de61
Jonas Dreßler <verdre@v0yd.nl>
2022-11-16 14:04:24 +0100

    clutter/test-utils: Add convenience function to emit pointer events
    This will allow for easily testing the behaviors of event emission.

--- src/tests/clutter/test-utils.h
+++ src/tests/clutter/test-utils.h
@@ -28,3 +28,34 @@ clutter_test_utils_create_texture_from_file (const char  *filename,
                        "content", image,
                        NULL);
 }
+
+static void
+clutter_test_utils_queue_pointer_event (ClutterStage     *stage,
+                                        ClutterEventType  type,
+                                        unsigned int      touch_slot,
+                                        float             x,
+                                        float             y)
+{
+  ClutterSeat *seat =
+    clutter_backend_get_default_seat (clutter_get_default_backend ());
+  ClutterInputDevice *pointer = clutter_seat_get_pointer (seat);
+  ClutterEvent *event = clutter_event_new (type);
+
+  clutter_event_set_coords (event, x, y);
+  clutter_event_set_device (event, pointer);
+  clutter_event_set_stage (event, stage);
+
+  if (type == CLUTTER_TOUCH_BEGIN ||
+      type == CLUTTER_TOUCH_UPDATE ||
+      type == CLUTTER_TOUCH_END ||
+      type == CLUTTER_TOUCH_CANCEL)
+    event->touch.sequence = GINT_TO_POINTER (touch_slot + 1);
+
+  /* Set the event as synthetic so that we don't try to do things like
+   * accepting/rejecting sequences with the xserver.
+   */
+  event->any.flags |= CLUTTER_EVENT_FLAG_SYNTHETIC;
+
+  clutter_event_put (event);
+  clutter_event_free (event);
+}
