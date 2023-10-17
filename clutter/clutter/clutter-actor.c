cd43ed01209abb738d597381e49a049a499b73f2
Ivan Molodetskikh <yalterz@gmail.com>
2023-10-17 10:44:00 +0400

    clutter/actor: Put concrete types into dynamic trace span names
    Makes it easier to see what's going on in the trace.

--- clutter/clutter/clutter-actor.c
+++ clutter/clutter/clutter-actor.c
@@ -3452,6 +3452,8 @@ clutter_actor_paint (ClutterActor        *self,
                                  "Clutter::Actor::paint()");
       COGL_TRACE_DESCRIBE (ClutterActorPaint,
                            _clutter_actor_get_debug_name (self));
+      COGL_TRACE_NAME_DYNAMIC (ClutterActorPaint,
+                               "%s::paint()", G_OBJECT_TYPE_NAME (self));
     }
 #endif
 
@@ -8665,6 +8667,8 @@ clutter_actor_allocate (ClutterActor          *self,
                                  "Clutter::Actor::allocate()");
       COGL_TRACE_DESCRIBE (ClutterActorAllocate,
                            _clutter_actor_get_debug_name (self));
+      COGL_TRACE_NAME_DYNAMIC (ClutterActorAllocate,
+                               "%s::allocate()", G_OBJECT_TYPE_NAME (self));
     }
 #endif
 
@@ -11634,6 +11638,7 @@ clutter_actor_event (ClutterActor       *actor,
 
   COGL_TRACE_BEGIN_SCOPED (Event, "Clutter::Actor::event()");
   COGL_TRACE_DESCRIBE (Event, _clutter_actor_get_debug_name (actor));
+  COGL_TRACE_NAME_DYNAMIC (Event, "%s::event()", G_OBJECT_TYPE_NAME (actor));
 
   g_object_ref (actor);
 
