prototypefund/gnome-verdre-mutter

clutter/timeline: Remove clutter_timeline_get_delta()
It's trivial for a user of ClutterTimeline to figure out the delta by
themselves, no need to make ClutterTimeline more complex.

Remove clutter_timeline_get_delta() and the internal msecs_delta
property. This allows making advancing the timeline a bit more
straightforward since we can now just pass the new delta via function
arguments.

clutter/clutter/clutter-transition-group.c
@@ -47,6 +47,8 @@
 
 struct _ClutterTransitionGroupPrivate
 {
+  int last_elapsed_time_ms;
+
   GHashTable *transitions;
 };
 
@@ -63,8 +65,8 @@ clutter_transition_group_new_frame (ClutterTimeline *timeline,
 
   priv = CLUTTER_TRANSITION_GROUP (timeline)->priv;
 
-  /* get the time elapsed since the last ::new-frame... */
-  msecs = clutter_timeline_get_delta (timeline);
+  msecs = elapsed - priv->last_elapsed_time_ms;
+  priv->last_elapsed_time_ms = elapsed;
 
   g_hash_table_iter_init (&iter, priv->transitions);
   while (g_hash_table_iter_next (&iter, &element, NULL))

warlice/mutter

clutter/clutter/clutter-transition-group.c
@@ -46,6 +46,7 @@
 struct _ClutterTransitionGroup
 {
   ClutterTransition parent_instance;
+  int last_elapsed_time_ms;
 
   GHashTable *transitions;
 };
@@ -61,8 +62,8 @@ clutter_transition_group_new_frame (ClutterTimeline *timeline,
   gpointer element;
   gint64 msecs;
 
-  /* get the time elapsed since the last ::new-frame... */
-  msecs = clutter_timeline_get_delta (timeline);
+  msecs = elapsed - transition->last_elapsed_time_ms;
+  transition->last_elapsed_time_ms = elapsed;
 
   g_hash_table_iter_init (&iter, transition->transitions);
   while (g_hash_table_iter_next (&iter, &element, NULL))