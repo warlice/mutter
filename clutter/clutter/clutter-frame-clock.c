warlice/mutter

commit 8f27ebf87eee6057992a90560d4118ab7bdf138d

clutter/frame-clock: Start next update ASAP after idle period
For frame updates in response to sporadic user interaction, this results
in input → output latency somewhere between the minimum possible and the
minimum plus the length of one display refresh cycle (assuming the frame
update can complete within a refresh cycle).

Applying a max_render_time based deadline which corresponds to higher
than the minimum possible latency would result in higher effective
minimum latency for sporadic user interaction.

This was discovered by Ivan Molodetskikh, based on measurements
described in https://mastodon.online/@YaLTeR/110848066454900941 .

v2:
* Set min_render_time_allowed_us = 0 as well, to avoid unthrottled
  frame events. (Robert Mader)

Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3174>

clutter/clutter/clutter-frame-clock.c
@@ -566,12 +566,25 @@ calculate_next_update_time_us (ClutterFrameClock *frame_clock,
         }
     }
 
-  while (next_presentation_time_us < now_us + min_render_time_allowed_us)
-    next_presentation_time_us += refresh_interval_us;
+  if (next_presentation_time_us != last_presentation_time_us + refresh_interval_us)
+    {
+      /* There was an idle period since the last presentation, so there seems
+       * be no constantly updating actor. In this case it's best to start
+       * working on the next update ASAP, this results in lowest average latency
+       * for sporadic user input.
+       */
+      next_update_time_us = now_us;
+      min_render_time_allowed_us = 0;
+    }
+  else
+    {
+      while (next_presentation_time_us < now_us + min_render_time_allowed_us)
+        next_presentation_time_us += refresh_interval_us;
 
-  next_update_time_us = next_presentation_time_us - max_render_time_allowed_us;
-  if (next_update_time_us < now_us)
-    next_update_time_us = now_us;
+      next_update_time_us = next_presentation_time_us - max_render_time_allowed_us;
+      if (next_update_time_us < now_us)
+        next_update_time_us = now_us;
+    }
 
   *out_next_update_time_us = next_update_time_us;
   *out_next_presentation_time_us = next_presentation_time_us;

commit c383a105675eeffcfcb5e520f11eebe0e3382d14

clutter/frame-clock: Detect idleness using last dispatch time
Presentation times only tell you when the display is busy, and not even
that for virtual monitors (f19f8fcb164ae).

By using the last dispatch time to detect frame clock idleness, we avoid
phase shifts from low frequency screen updates occurring concurrently
with high frequency client callbacks (#3559).

This solution works equally well for double or triple buffering.

Closes: https://gitlab.gnome.org/GNOME/mutter/-/issues/3559
Fixes: 8f27ebf87eee6057992a90560d4118ab7bdf138d

clutter/clutter/clutter-frame-clock.c
@@ -606,10 +606,9 @@ calculate_next_update_time_us (ClutterFrameClock *frame_clock,
         }
     }
 
-  if (frame_clock->last_presentation_flags & CLUTTER_FRAME_INFO_FLAG_VSYNC &&
-      next_presentation_time_us != last_presentation_time_us + refresh_interval_us)
+  if (frame_clock->last_dispatch_time_us < (now_us - 2 * refresh_interval_us))
     {
-      /* There was an idle period since the last presentation, so there seems
+      /* There was an idle period since the last dispatch, so there seems
        * be no constantly updating actor. In this case it's best to start
        * working on the next update ASAP, this results in lowest average latency
        * for sporadic user input.