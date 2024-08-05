warlice/mutter

commit 536c52bdb10c195ddaa85541814ef7537db41eb3

wayland/drm-lease: Advertise initial devices
Advertise one wp_drm_lease_device_v1 global for each DRM node.

Co-authored-by: José Expósito <jexposit@redhat.com>
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3746>

src/wayland/meta-wayland-drm-lease.c
@@ -1,0 +1,208 @@
+/*
+ * Copyright (C) 2016 Red Hat Inc.
+ * Copyright (C) 2017 Intel Corporation
+ * Copyright (C) 2018,2019 DisplayLink (UK) Ltd.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ *
+ */
+
+#include "config.h"
+
+#include "wayland/meta-wayland-drm-lease.h"
+
+#include <glib.h>
+
+#include "backends/native/meta-backend-native.h"
+#include "backends/native/meta-drm-lease.h"
+#include "backends/native/meta-kms-connector.h"
+#include "backends/native/meta-kms-device.h"
+#include "backends/native/meta-kms-device-private.h"
+#include "backends/native/meta-kms-impl-device.h"
+#include "backends/native/meta-kms.h"
+#include "backends/edid.h"
+#include "wayland/meta-wayland-private.h"
+
+#include "drm-lease-v1-server-protocol.h"
+
+struct _MetaWaylandDrmLeaseManager
+{
+  MetaWaylandCompositor *compositor;
+  MetaDrmLeaseManager *drm_lease_manager;
+
+  /* Key:   MetaKmsDevice *kms_device
+   * Value: MetaWaylandDrmLeaseDevice *lease_device
+   */
+  GHashTable *devices;
+};
+
+typedef struct _MetaWaylandDrmLeaseDevice
+{
+  MetaWaylandDrmLeaseManager *lease_manager;
+
+  struct wl_global *global;
+  MetaKmsDevice *kms_device;
+
+  GList *resources;
+} MetaWaylandDrmLeaseDevice;
+
+static void
+meta_wayland_drm_lease_device_free (MetaWaylandDrmLeaseDevice *lease_device)
+{
+  g_object_unref (lease_device->kms_device);
+}
+
+static void
+meta_wayland_drm_lease_device_release (MetaWaylandDrmLeaseDevice *lease_device)
+{
+  g_rc_box_release_full (lease_device,
+                         (GDestroyNotify) meta_wayland_drm_lease_device_free);
+}
+
+G_DEFINE_AUTOPTR_CLEANUP_FUNC (MetaWaylandDrmLeaseDevice,
+                               meta_wayland_drm_lease_device_release);
+
+static void
+wp_drm_lease_device_create_lease_request (struct wl_client   *client,
+                                          struct wl_resource *resource,
+                                          uint32_t            id)
+{
+}
+
+static void
+wp_drm_lease_device_release (struct wl_client   *client,
+                             struct wl_resource *resource)
+{
+  wl_resource_destroy (resource);
+}
+
+static const struct wp_drm_lease_device_v1_interface drm_lease_device_implementation = {
+  wp_drm_lease_device_create_lease_request,
+  wp_drm_lease_device_release,
+};
+
+static void
+wp_drm_lease_device_destructor (struct wl_resource *resource)
+{
+  MetaWaylandDrmLeaseDevice *lease_device =
+    wl_resource_get_user_data (resource);
+
+  lease_device->resources = g_list_remove (lease_device->resources, resource);
+  meta_wayland_drm_lease_device_release (lease_device);
+}
+
+static void
+lease_device_bind (struct wl_client *client,
+                   void             *user_data,
+                   uint32_t          version,
+                   uint32_t          id)
+{
+  MetaWaylandDrmLeaseDevice *lease_device = user_data;
+  struct wl_resource *resource;
+
+  resource = wl_resource_create (client, &wp_drm_lease_device_v1_interface,
+                                 version, id);
+  wl_resource_set_implementation (resource,
+                                  &drm_lease_device_implementation,
+                                  g_rc_box_acquire (lease_device),
+                                  wp_drm_lease_device_destructor);
+
+  lease_device->resources = g_list_prepend (lease_device->resources, resource);
+}
+
+static MetaWaylandDrmLeaseDevice *
+meta_wayland_drm_lease_device_new (MetaWaylandDrmLeaseManager *lease_manager,
+                                   MetaKmsDevice              *kms_device)
+{
+  struct wl_display *wayland_display =
+    meta_wayland_compositor_get_wayland_display (lease_manager->compositor);
+  MetaWaylandDrmLeaseDevice *lease_device;
+
+  lease_device = g_rc_box_new0 (MetaWaylandDrmLeaseDevice);
+  lease_device->lease_manager = lease_manager;
+  lease_device->kms_device = g_object_ref (kms_device);
+  lease_device->global = wl_global_create (wayland_display,
+                                           &wp_drm_lease_device_v1_interface,
+                                           META_WP_DRM_LEASE_DEVICE_V1_VERSION,
+                                           lease_device,
+                                           lease_device_bind);
+
+  return lease_device;
+}
+
+static void
+meta_wayland_drm_lease_manager_add_device (MetaKmsDevice              *kms_device,
+                                           MetaWaylandDrmLeaseManager *lease_manager)
+{
+  g_autoptr (MetaWaylandDrmLeaseDevice) lease_device = NULL;
+
+  lease_device = meta_wayland_drm_lease_device_new (lease_manager, kms_device);
+  g_hash_table_insert (lease_manager->devices,
+                       kms_device,
+                       g_steal_pointer (&lease_device));
+}
+
+static MetaWaylandDrmLeaseManager *
+meta_wayland_drm_lease_manager_new (MetaWaylandCompositor *compositor)
+{
+  MetaContext *context = meta_wayland_compositor_get_context (compositor);
+  MetaBackend *backend = meta_context_get_backend (context);
+  MetaBackendNative *backend_native;
+  MetaKms *kms;
+  MetaWaylandDrmLeaseManager *lease_manager;
+  MetaDrmLeaseManager *drm_lease_manager;
+
+  if (!META_IS_BACKEND_NATIVE (backend))
+    return NULL;
+
+  backend_native = META_BACKEND_NATIVE (backend);
+  kms = meta_backend_native_get_kms (backend_native);
+  drm_lease_manager = g_object_new (META_TYPE_DRM_LEASE_MANAGER,
+                                    "meta-kms", kms,
+                                    NULL);
+
+  lease_manager = g_new0 (MetaWaylandDrmLeaseManager, 1);
+  lease_manager->compositor = compositor;
+  lease_manager->drm_lease_manager = drm_lease_manager;
+  lease_manager->devices =
+    g_hash_table_new_full (NULL, NULL,
+                           NULL,
+                           (GDestroyNotify) meta_wayland_drm_lease_device_release);
+
+  g_list_foreach (meta_drm_lease_manager_get_devices (drm_lease_manager),
+                  (GFunc) meta_wayland_drm_lease_manager_add_device,
+                  lease_manager);
+
+  return lease_manager;
+}
+
+static void
+meta_wayland_drm_lease_manager_free (gpointer data)
+{
+  MetaWaylandDrmLeaseManager *lease_manager = data;
+
+  g_clear_pointer (&lease_manager->devices, g_hash_table_unref);
+  g_clear_pointer (&lease_manager->drm_lease_manager, g_object_unref);
+  g_free (lease_manager);
+}
+
+void
+meta_wayland_drm_lease_manager_init (MetaWaylandCompositor *compositor)
+{
+  g_object_set_data_full (G_OBJECT (compositor), "-meta-wayland-drm-lease",
+                          meta_wayland_drm_lease_manager_new (compositor),
+                          meta_wayland_drm_lease_manager_free);
+}

src/meson.build
@@ -725,6 +725,13 @@ if have_wayland
       'wayland/meta-xwayland-surface.h',
     ]
   endif
+
+  if have_native_backend
+    mutter_sources += [
+      'wayland/meta-wayland-drm-lease.c',
+      'wayland/meta-wayland-drm-lease.h',
+    ]
+  endif
 endif
 
 if have_native_backend
@@ -1066,6 +1073,7 @@ if have_wayland
   #  - protocol stability ('private', 'stable' or 'unstable')
   #  - protocol version (if stability is 'unstable')
   wayland_protocols = [
+    ['drm-lease', 'staging', 'v1', ],
     ['fractional-scale', 'staging', 'v1', ],
     ['gtk-shell', 'private', ],
     ['idle-inhibit', 'unstable', 'v1', ],

src/wayland/meta-wayland-drm-lease.h
@@ -1,0 +1,25 @@
+/*
+ * Copyright (C) 2021 Red Hat Inc.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ *
+ */
+
+#pragma once
+
+#include "wayland/meta-wayland-types.h"
+
+void meta_wayland_drm_lease_manager_init (MetaWaylandCompositor *compositor);

src/wayland/meta-wayland-types.h
@@ -71,3 +71,5 @@ typedef struct _MetaWaylandXdgForeign MetaWaylandXdgForeign;
 typedef struct _MetaWaylandFilterManager MetaWaylandFilterManager;
 
 typedef struct _MetaWaylandClient MetaWaylandClient;
+
+typedef struct _MetaWaylandDrmLeaseManager MetaWaylandDrmLeaseManager;

src/wayland/meta-wayland-versions.h
@@ -58,3 +58,4 @@
 #define META_MUTTER_X11_INTEROP_VERSION 1
 #define META_WP_FRACTIONAL_SCALE_VERSION 1
 #define META_XDG_DIALOG_VERSION 1
+#define META_WP_DRM_LEASE_DEVICE_V1_VERSION 1

commit 90acc720cab8635621d2609f7f4b404b181f0cde

wayland/drm-lease: Check drm fd before binding devices
The DRM lease protocol doesn't specify a way to report issues opening a
non-master fd for a DRM node when the client binds to the DRM lease
device.

To mitigate this issue do not bind the device when it is not possible to
open a non-privileged DRM fd during device creation.

Fixes: 536c52bdb10c ("wayland/drm-lease: Advertise initial devices")

src/wayland/meta-wayland-drm-lease.c
@@ -564,6 +564,16 @@ meta_wayland_drm_lease_device_new (MetaWaylandDrmLeaseManager *lease_manager,
   MetaDrmLeaseManager *drm_lease_manager = lease_manager->drm_lease_manager;
   MetaWaylandDrmLeaseDevice *lease_device;
   g_autoptr (GList) kms_connectors = NULL;
+  MetaKmsImplDevice *impl_device;
+  g_autofd int fd = -1;
+
+  impl_device = meta_kms_device_get_impl_device (kms_device);
+  fd = meta_kms_impl_device_open_non_privileged_fd (impl_device);
+  if (fd < 0)
+    {
+      g_warning ("Error getting non privileged fd, ignoring DRM lease device");
+      return NULL;
+    }
 
   lease_device = g_rc_box_new0 (MetaWaylandDrmLeaseDevice);
   lease_device->lease_manager = lease_manager;
@@ -596,9 +606,12 @@ meta_wayland_drm_lease_manager_add_device (MetaKmsDevice              *kms_devic
   g_autoptr (MetaWaylandDrmLeaseDevice) lease_device = NULL;
 
   lease_device = meta_wayland_drm_lease_device_new (lease_manager, kms_device);
-  g_hash_table_insert (lease_manager->devices,
-                       kms_device,
-                       g_steal_pointer (&lease_device));
+  if (lease_device)
+    {
+      g_hash_table_insert (lease_manager->devices,
+                           kms_device,
+                           g_steal_pointer (&lease_device));
+    }
 }
 
 static void