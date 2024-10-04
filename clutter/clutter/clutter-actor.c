00e1040e8ca5aacfbbce2abc221fc7f989f9e0e7
Marco Trevisan (Treviño) <mail@3v1n0.net>
2024-10-04 21:37:30 +0200

    clutter/actor: Make child position changes a noop when possible
    In set_child_{above,below}_sibling and set_child_at_index() functions we
    may end up adding/removing children or queuing relayouts or triggering
    toggle references (leading to gjs unneeded work) for no reasons.
    
    So avoid moving the actor children in cases we're already all set

--- clutter/clutter/clutter-actor.c
+++ clutter/clutter/clutter-actor.c
@@ -482,6 +482,7 @@
 #include <math.h>
 
 #include <gobject/gvaluecollector.h>
+#include <string.h>
 
 #include "cogl/cogl.h"
 
@@ -11644,6 +11645,14 @@ clutter_actor_set_child_above_sibling (ClutterActor *self,
       (sibling != NULL && CLUTTER_ACTOR_IN_DESTRUCTION (sibling)))
     return;
 
+  /* The child is already the last */
+  if (sibling == NULL && self->priv->last_child == child)
+    return;
+
+  /* The child is already above the sibling */
+  if (sibling != NULL && child->priv->prev_sibling == sibling)
+    return;
+
   /* we don't want to change the state of child, or emit signals, or
    * regenerate ChildMeta instances here, but we still want to follow
    * the correct sequence of steps encoded in remove_child() and
@@ -11694,6 +11703,14 @@ clutter_actor_set_child_below_sibling (ClutterActor *self,
       (sibling != NULL && CLUTTER_ACTOR_IN_DESTRUCTION (sibling)))
     return;
 
+  /* The child is already the first */
+  if (sibling == NULL && self->priv->first_child == child)
+    return;
+
+  /* The child is already below the sibling */
+  if (sibling != NULL && child->priv->next_sibling == sibling)
+    return;
+
   /* see the comment in set_child_above_sibling() */
   g_object_ref (child);
   clutter_actor_remove_child_internal (self, child, 0);
@@ -11730,6 +11747,9 @@ clutter_actor_set_child_at_index (ClutterActor *self,
 
   if (CLUTTER_ACTOR_IN_DESTRUCTION (self) ||
       CLUTTER_ACTOR_IN_DESTRUCTION (child))
+    return;
+
+  if (clutter_actor_get_child_at_index (self, index_) == child)
     return;
 
   g_object_ref (child);
